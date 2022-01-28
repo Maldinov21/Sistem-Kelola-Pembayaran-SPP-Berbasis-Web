016187e68561c06a6de550c7ea046b5e5fcf2b47
Maldinov21 <maldinovriansyah21@gmail.com>
2022-01-28 11:06:02 +0700

    Sistem-Kelola-Pembayaran-SPP-Berbasis-Web

---
+++ application/config/arc.php
@@ -0,0 +1,41 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Media
+|--------------------------------------------------------------------------
+| Konfigurasi untuk lokasi media
+| 
+|
+*/
+$config['media_url'] = '-';
+
+/*
+|--------------------------------------------------------------------------
+| Setting
+|--------------------------------------------------------------------------
+|
+| 
+|
+*/
+$config['app_name'] = '';
+$config['version'] = 'V.1';
+$config['created'] = 'Copyright © ' . date('Y');
+
+/*
+|--------------------------------------------------------------------------
+| Email
+|--------------------------------------------------------------------------
+|
+| Configuration email to send email notification
+|
+*/
+$config['email'] = TRUE;
+
+/*
+|--------------------------------------------------------------------------
+| Email disclaimer
+|--------------------------------------------------------------------------
+|
+*/
+$config['email_disclaimer'] = "\n\n<em>Email ini dikirim secara otomatis, mohon untuk tidak membalas email ini. Jika mengalami kendala teknis maka dapat menghubungi <a href=\"mailto:\">email@gmail.com</a></em>";
